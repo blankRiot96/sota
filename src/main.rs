7556054120df3beb59a076ad1378cbdee0aa0222
Axis <77634274+blankRiot96@users.noreply.github.com>
2022-07-30 15:58:47 +0530

    small changes

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@ use yew::prelude::*;
 
 enum Msg {
     AddOne,
-    AddTwo
+    AddTwo,
 }
 
 struct CounterComponent {
     count: i64,
 }
-
 
 impl Component for CounterComponent {
     type Message = Msg;
@@ -22,11 +21,11 @@ impl Component for CounterComponent {
         match msg {
             Msg::AddOne => {
                 self.count += 1;
-                true  // Re-render component 
-            },
+                true // Re-render component
+            }
             Msg::AddTwo => {
                 self.count += 2;
-                true 
+                true
             }
         }
     }
@@ -37,13 +36,14 @@ impl Component for CounterComponent {
             <div class="container">
                 <p>{self.count}</p>
                 <button class="bg-blue-500 hover:bg-blue-300 px-2 py-2 rounded"
-                onclick={link.callback(|_| Msg::AddTwo)}>{"Click me"}</button>
+                onclick={link.callback(|_| Msg::AddTwo)}>{ "+2" }</button>
 
-                <button 
+                <button
                 class="bg-blue-500 hover:bg-blue-300 px-2 py-2 rounded"
                 onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
             </div>
         }
+
     }
 }
 
